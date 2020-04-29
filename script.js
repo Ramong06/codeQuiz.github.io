96b1d64551775f2fda88c997b0b000621e436a81
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-04-29 17:08:04 -0500

    Questions and Answers entered

---
+++ script.js
@@ -0,0 +1,44 @@
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const qImg = document.getElementById("questionImage");
+const counter =document.getElementById("question");
+const timeGauge = document.getElementById("timeGauge");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+
+const progress = document.getElementById("progress");
+const scoreContainer = document.getElementById("scoreContainer");
+
+let questions = [
+    {
+        question : "What does HTML stand for?",
+        choiceA : "Hyperlinks and Tested Markup Language",
+        choiceB : "Hyper Text Markup Language",
+        choiceC : "Hand Tools Made for Lefties",
+        correct : "B"
+    },
+    {
+        question : "What does CSS stand for?",
+        choiceA : "Cascading Style Sheets ",
+        choiceB : "Cool Stuff Sheet",
+        choiceC : "Compilied Styling Sheets",
+        correct : "A"  
+    }
+    {
+        question : "In Javascript. What is another way to type \"if (var === true) {};\" ",
+        choiceA : "if (!var) {};",
+        choiceB : "else (var ==> 0); {}",
+        choiceC : "if (var) {};",
+        correct : "C"
+    }
+    {
+        question : "*BONUS* What is my cat's name?",
+        choiceA : "Jack",
+        choiceB : "Lil' Panther Boii",
+        choiceC : "Both A and B",
+        correct : "C"
+    }
+
+]
