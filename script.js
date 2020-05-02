c11a4788e563e911aa340e7755fd46c60e641188
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-05-01 21:45:15 -0500

    re-doing with different HTML technique and utilizing CSS

--- script.js
+++ script.js
@@ -1,54 +1,34 @@
-const start = document.getElementById("start");
+const start = document.getElementById("#start");
 const quiz = document.getElementById("quiz");
 const qImg = document.getElementById("questionImage");
-const counter =document.getElementById("question");
+const question = document.getElementById("#question");
 const timeGauge = document.getElementById("timeGauge");
 
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
+const choiceA = document.getElementById("#A");
+const choiceB = document.getElementById("#B");
+const choiceC = document.getElementById("#C");
 
 const progress = document.getElementById("progress");
 const scoreContainer = document.getElementById("scoreContainer");
 
-let quizQuestions = [
-    {
-        question : "What does HTML stand for?",
-        choiceA : "Hyperlinks and Tested Markup Language",
-        choiceB : "Hyper Text Markup Language",
-        choiceC : "Hand Tools Made for Lefties",
-        correct : "B"
-    },
-    {
-        question : "What does CSS stand for?",
-        choiceA : "Cascading Style Sheets ",
-        choiceB : "Cool Stuff Sheet",
-        choiceC : "Compilied Styling Sheets",
-        correct : "A"  
-    },
-    {
-        question : "In Javascript. What is another way to type \"if (var === true) {};\" ",
-        choiceA : "if (!var) {};",
-        choiceB : "else (var ==> 0); {}",
-        choiceC : "if (var) {};",
-        correct : "C"
-    },
-    {
-        question : "In Javascript. How can I declare \"var x = 5;\" inside ONE specific block if \"var x = 10;\" has been declared globally?",
-        choiceA : "let = 5;",
-        choiceB : "else if (var = 5);",
-        choiceC : "Both A and B are correct",
-        correct : "A"  
-    },
-    {
-        question : "*BONUS* What is my cat's name?",
-        choiceA : "Jack",
-        choiceB : "Lil' Panther Boii",
-        choiceC : "Both A and B",
-        correct : "C"
-    }
+let lastQuestionIndex = quizQuestions.length - 1;
+let runningQuestionIndex = 0;
 
-]
+
+function renderQuestion() {
+    var q = quizQuestions[runningQuestionIndex];
+    question.innerHTML = q.question;
+    choiceA.textContent = q.choiceA;
+    choiceB.textContent = q.choiceB;
+    choiceC.textContent = q.choiceC;
+
+    runningQuestionIndex++;
+
+}
+
+renderQuestion();
+
+
 
 
 //Javascript delay function will be used for this homework assignment!
